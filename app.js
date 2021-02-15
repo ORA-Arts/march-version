4a406e7d431ce18a35034505c3fb45c0b8dbae0b
Patrick Lehmann <ptrcklehmann@gmail.com>
2021-02-15 12:09:11 +0100

    create login (view) and passport set up

--- app.js
+++ app.js
@@ -18,6 +18,70 @@ const app = express();
 // ℹ️ This function is getting exported from the config folder. It runs most middlewares
 require("./config")(app);
 
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session)
+
+const mongoose = require('.db/index')
+
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET,
+        cookie: { maxAge: 1000 * 60 * 60 * 24 },
+        saveUninitialized: false,
+        resave: true,
+        store: new MongoStore({
+          mongooseConnection: mongoose.connection
+        })
+      })
+)
+
+// passport configuration
+const User = require('./models/User.model')
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+const bcrypt = require('bcrypt')
+
+// we serialize only the `_id` field of the user to keep the information stored minimum
+passport.serializeUser((user, done) => {
+    done(null, user._id)
+})
+
+// when we need the information for the user, the deserializeUser function is called with the id that we previously serialized to fetch the user from the database
+passport.deserializeUser((id, done) => {
+    User.findById(id)
+        .then(dbUser => {
+        done(null, dbUser)
+        })
+        .catch(err => {
+        done(err);
+        })
+})
+
+passport.use(
+    new LocalStrategy((email, password, done) => {
+      // login
+      User.findOne({ email: email })
+        .then(userFromDB => {
+          if (userFromDB === null) {
+            // there is no user with this username
+            done(null, false, { message: 'Wrong Credentials' });
+          } else if (!bcrypt.compareSync(password, userFromDB.password)) {
+            // the password is not matching
+            done(null, false, { message: 'Wrong Credentials' });
+          } else {
+            // the userFromDB should now be logged in
+            done(null, userFromDB)
+          }
+        })
+        .catch(err => {
+          console.log(err);
+        })
+    })
+  )
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 // default value for title local
 const projectName = "march-version";
 const capitalized = (string) => string[0].toUpperCase() + string.slice(1).toLowerCase();
--- routes/auth.js
+++ routes/auth.js
@@ -1,42 +1,41 @@
-const router = require("express").Router();
-const User = require('../models/User.model');
-const bcrypt = require('bcrypt');
+const router = require("express").Router()
+const User = require('../models/User.model')
+const bcrypt = require('bcrypt')
 
 // GET login
 router.get('/login', (req, res, next) => {
-    res.render('login');
-  });
+    res.render('login')
+});
 
 // POST login
 router.post('/login', (req, res) => {
-    const { username, password } = req.body;
+    const { email, password } = req.body
     // check if we have a user with the entered username
-    User.findOne({ username: username })
+    User.findOne({ email: email })
       .then(userFromDB => {
         if (userFromDB === null) {
           // if not we show login again
-          res.render('login', { message: 'Invalid credentials' });
+          res.render('login', { message: 'Invalid credentials' })
           return;
         }
         // if username is existing then we want to check the password
         if (bcrypt.compareSync(password, userFromDB.password)) {
           // password and hash match
           // now we want to log the user in
-          req.session.user = userFromDB;
-          res.redirect('/profile');
+          req.session.user = userFromDB
+          res.redirect('editorial/index')
         } else {
-          res.render('login', { message: 'Invalid credentials' });
+          res.render('login', { message: 'Invalid credentials' })
         }
       })
-  
-  })
+})
 
 // GET logout
 router.get('/logout', (req, res) => {
     // logout() is a passport function
     req.logout();
-    res.redirect('/');
+    res.redirect('/')
   })
 
-module.exports = router;
+module.exports = router
 
