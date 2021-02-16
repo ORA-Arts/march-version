40d8cf9697118b009ea1a82cad735bbedcba4326
Louisiane_Berlin <corentine.piette@gmail.com>
2021-02-16 11:47:47 +0100

    functionnality working

--- routes/index.js
+++ routes/index.js
@@ -75,8 +75,36 @@ transporter.verify(function (error, success) {
   }
 });
 
+router.post("/contact-us", (req, res) => {
+  //1.
+  let form = new multiparty.Form();
+  let data = {};
+  form.parse(req, function (err, fields) {
+    console.log(fields);
+    Object.keys(fields).forEach(function (property) {
+      data[property] = fields[property].toString();
+    });
 
+    //2. You can configure the object however you want
+    const mail = {
+      from: data.name,
+      to: process.env.EMAIL,
+      subject: data.object,
+      text: `${data.name} <${data.email}> \n${data.body}`,
+    };
 
+    //3.
+    transporter.sendMail(mail, (err, data) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send("Something went wrong.");
+      } else {
+        res.status(200).send("Email successfully sent to recipient!");
+      }
+    });
+  });
+});
+  
 module.exports = router;
 
 
