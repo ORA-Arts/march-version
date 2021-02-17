1349427c761b4005dfa876fe1ec5b29ae884cac2
Patrick Lehmann <ptrcklehmann@gmail.com>
2021-02-17 18:05:21 +0100

    update editorial edit

--- routes/editorial.js
+++ routes/editorial.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const Editorial = require('../models/Editorial.model')
 const { uploader, cloudinary } = require('../config/cloudinary');
+const { findById } = require("../models/Editorial.model");
 // add edit delete view Editorial view
 // GET editorial
 
@@ -46,14 +47,33 @@ router.get('/:id/edit', (req, res, next) => {
         });
 })
 
-router.post('/:id/edit', (req, res, next) => {
+router.put('/:id/edit', uploader.single('photo'), (req, res, next) => {
+    
     console.log(req.body);
+    let post = Editorial.findById(req.params.id)
+    cloudinary.uploader.destroy(post.publicId)
+
     const { title, category, subtitle, text, author } = req.body
     const inFocus = (req.body.inFocus === 'true') ? true : false
-    const imgName = req.file.originalname
-    const imgPath = req.file.path
-    const publicId = req.file.filename
-    Editorial.findByIdAndUpdate(req.params.id, { title: title, category: category, subtitle: subtitle, text: text, author: author, inFocus: inFocus, imgName: imgName, imgPath: imgPath, publicId: publicId })
+    const imgName = () => {
+        if (req.file.originalname != null) {
+        cloudinary.uploader.destroy(post.publicId)
+        return req.file.originalname
+        } 
+        else return prevPost.imgName
+    }
+    const postData = {
+        title: title,
+        category: category, 
+        subtitle: subtitle, 
+        text: text, 
+        author: author, 
+        inFocus: inFocus, 
+        imgName: imgName, imgPath: imgPath, publicId: publicId
+    }
+    const imgPath = (req.file.path) ? req.file.path : prevPost.imgPath
+    const publicId = (req.file.filename) ? req.file.filename : prevPost.publicId
+    Editorial.findByIdAndUpdate(req.params.id, postData, {new: true})
     .then(() => {
       res.redirect('/editorial');
     })
