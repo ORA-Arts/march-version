5757e1fa292a941cf2c1a6ae0cd5dea7dba02424
Louisiane_Berlin <corentine.piette@gmail.com>
2021-02-17 14:08:46 +0100

    with password in the URL

--- db/index.js
+++ db/index.js
@@ -1,11 +1,10 @@
 // ℹ️ package responsible to make the connection with mongodb
 // https://www.npmjs.com/package/mongoose
 const mongoose = require("mongoose");
-
+require("dotenv/config");
 // ℹ️ Sets the MongoDB URI for our app to have access to it.
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
-const MONGODB_URI = "mongodb+srv://cpiette:<Mongo369*>@cluster0.y17dc.mongodb.net/<dbname>?retryWrites=true&w=majority";
 const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/march-version";
 
 mongoose
